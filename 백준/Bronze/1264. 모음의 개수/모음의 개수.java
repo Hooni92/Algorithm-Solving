2284de5e406624247774e53cc55700f5ff23f2b5
Hooni92 <108508922+Hooni92@users.noreply.github.com>
2023-03-31 02:13:10 +0900

    [Bronze IV] Title: 모음의 개수, Time: 216 ms, Memory: 17888 KB -BaekjoonHub

--- 백준/Bronze/1264. 모음의 개수/모음의 개수.java
+++ 백준/Bronze/1264. 모음의 개수/모음의 개수.java
@@ -4,18 +4,19 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		while(true) {
-			String text = sc.nextLine();
-			if (text.equals("#")) {
+			String text =  sc.nextLine();
+			if(text.equals("#")) {
 				break;
-			}
-			int count = 0;
-			for(int i=0; i < text.length(); i++) {
-				char imsi = text.toLowerCase().charAt(i);
-				if (imsi == 'a' || imsi == 'e' || imsi == 'i' ||  imsi == 'o' || imsi == 'u' ) {
-					count ++;
+			}else {
+				int count = 0;
+				for(int i = 0; i<text.length(); i++) {
+					char alphabet = text.toLowerCase().charAt(i);
+					if(alphabet == 'a' || alphabet == 'e' || alphabet == 'i' || alphabet == 'o' || alphabet == 'u') {
+						count ++;
+					}
 				}
+				System.out.println(count);
 			}
-			System.out.println(count);
 		}
 		sc.close();
 	}
